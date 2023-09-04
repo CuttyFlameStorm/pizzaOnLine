dbde0200f7a19b4f91bdef0b2920b0f1c4dc56a3
Sidath SOEUN <sidath.soeun2023@campus-eni.fr>
2023-09-04 14:25:56 +0200

    recupération de la liste des pizzas en Base SQL et intégration de la liste dans la vue via le controller

--- src/main/java/fr/eni/pizzaOnLine/controller/HomeController.java
+++ src/main/java/fr/eni/pizzaOnLine/controller/HomeController.java
@@ -1,18 +1,25 @@
 package fr.eni.pizzaOnLine.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.eni.pizzaOnLine.service.ProduitService;
 
 @Controller
 @RequestMapping("/carte")
 public class HomeController {
 
+	@Autowired
+	private ProduitService produitService;
+	
 	@GetMapping
-	public String tousLesProduits() {
-		
-		
+	public String tousLesProduits(Model model) {
+		// recuperer la liste de produits et le transmettre à la vue
+		model.addAttribute("produits", produitService.consulterProduits());		
 		return "carte";
 	}
 	
